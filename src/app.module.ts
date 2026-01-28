f1fefb4df6e858fb4badff4f0486967c6cb1b4fe
kangbaekho <kbh081004@gmail.com>
2026-01-28 22:03:54 +0900

    feat: Logger 미들웨어 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { OrderModule } from './order/order.module';
 import { PrismaModule } from './common/prisma/prisma.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
     imports: [PrismaModule, OrderModule],
     controllers: [],
     providers: [],
 })
-export class AppModule {}
+export class AppModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(LoggerMiddleware).forRoutes('*');
+    }
+}
