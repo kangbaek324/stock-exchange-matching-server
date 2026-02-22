386e7c23c5a592091bae01807288968310727b2e
kangbaekho <kbh081004@gmail.com>
2026-02-22 21:51:14 +0900

    feat: 헬스체크 API 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { OrderModule } from './order/order.module';
 import { PrismaModule } from './common/prisma/prisma.module';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
 
 @Module({
     imports: [PrismaModule, OrderModule, ConfigModule.forRoot({ isGlobal: true })],
-    controllers: [],
+    controllers: [AppController],
     providers: [],
 })
 export class AppModule {
